6f08d29fd7cda26b02d5052051b540cf4bd5090b
rizkiwnf <rizkiwahyunf2@gmail.com>
2023-12-25 12:29:11 +0700

    task 10

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from "@/layout";
 import { useEffect } from "react";
+import Image from "next/image";
 
 export default function Main({ children }) {
 
@@ -12,8 +13,14 @@ export default function Main({ children }) {
   return (
     <>
       <Layout metaTitle="Home" metaDescription="Ini Home">
-        <div className="content container mx-auto w-32">
+        <div className="content container mx-auto  flex  flex-col justify-center">
           <p className="text-center bg-purple-300 rounded-md py-3 mb-2 text-white font-semibold">Home</p>
+          <div className="flex flex-col md:flex-row gap-5 justify-center mb-5">
+            {/* render using tag image bawaan html */}
+            <img src="/cat.jpg" style={{ width: 300, height: 300 }} alt="cat" />
+            {/* render using comp bawaan next js */}
+            <Image src="/cat.jpg" width={300} height={300} alt="cat" />
+          </div>
         </div>
       </Layout>
     </>
